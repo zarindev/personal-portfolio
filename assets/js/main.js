d0e231c66115954d836b093d9ed681cb56330a28
Md Zarin <mdzarinjerry@gmail.com>
2021-10-27 04:08:23 +0600

    new commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,13 +35,40 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*Accordion <Skills></Skills>
+/*Accordion skills*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills);
+})
 
 
-/*Qualification <Tabs></Tabs>
+
+
+
+
+/*Qualification tabs*/
 
 
 
 /*Services Modal*/
 
+/*Portfolio Swiper*/
 
+/*Testimonial*/
+
+/*Scroll Sections Active Link*/
+
+/*Change Background Header*/
