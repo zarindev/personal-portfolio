97eb773c4ccf6c7220031b02febdff7d30a30130
Md Zarin <mdzarinjerry@gmail.com>
2021-10-26 06:00:50 +0600

    new items

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,14 @@ if(navClose){
 
 
 /*Remove Menu Mobile*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*Accordion <Skills></Skills>
 
